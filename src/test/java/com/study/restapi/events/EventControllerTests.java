8fb31210fdc8a93bfeb91f207ae2080c69295860
LEE <jae_choel@naver.com>
2021-12-19 20:15:59 +0900

    Spring Rest Docs

--- src/test/java/com/study/restapi/events/EventControllerTests.java
+++ src/test/java/com/study/restapi/events/EventControllerTests.java
@@ -1,20 +1,29 @@
 package com.study.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.restapi.common.RestDocsConfiguration;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,6 +32,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //@WebMvcTest // 단위 테스트
 @SpringBootTest // 통합테스트
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 public class EventControllerTests {
   @Autowired
   private MockMvc mockMvc;
@@ -67,6 +78,54 @@ public class EventControllerTests {
       .andExpect(jsonPath("_links.self").exists())
       .andExpect(jsonPath("_links.query-events").exists())
       .andExpect(jsonPath("_links.update-event").exists())
+      .andDo(document("create-event",
+        links(
+          linkWithRel("self").description("link to self"),
+          linkWithRel("query-events").description("link to query events"),
+          linkWithRel("update-event").description("link to update an existing event")
+        ),
+        requestHeaders(
+          headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+          headerWithName(HttpHeaders.CONTENT_TYPE).description("content-type header")
+        ),
+        requestFields(
+          fieldWithPath("name").description("Name of New Event"),
+          fieldWithPath("description").description("Description of New Event"),
+          fieldWithPath("beginEnrollmentDateTime").description("beginEnrollmentDateTime of New Event"),
+          fieldWithPath("closeEnrollmentDateTime").description("closeEnrollmentDateTime of New Event"),
+          fieldWithPath("beginEventDateTime").description("beginEventDateTime of New Event"),
+          fieldWithPath("endEventDateTime").description("endEventDateTime of New Event"),
+          fieldWithPath("location").description("location of New Event"),
+          fieldWithPath("basePrice").description("basePrice of New Event"),
+          fieldWithPath("maxPrice").description("maxPrice of New Event"),
+          fieldWithPath("limitOfEnrollment").description("limitOfEnrollment of New Event")
+        ),
+        responseHeaders(
+          headerWithName(HttpHeaders.LOCATION).description("LOCATION header"),
+          headerWithName(HttpHeaders.CONTENT_TYPE).description("content-type header")
+        ),
+        responseFields(
+          // link를 명시했는데 에러가 발생함 (response에 _links 가 있기 때문) -> relaxedResponseFields 사용하면 됨.
+          // 근데 다 명시하는 거를 추천함.
+          fieldWithPath("id").description("identifier of New Event"),
+          fieldWithPath("name").description("Name of New Event"),
+          fieldWithPath("description").description("Description of New Event"),
+          fieldWithPath("beginEnrollmentDateTime").description("beginEnrollmentDateTime of New Event"),
+          fieldWithPath("closeEnrollmentDateTime").description("closeEnrollmentDateTime of New Event"),
+          fieldWithPath("beginEventDateTime").description("beginEventDateTime of New Event"),
+          fieldWithPath("endEventDateTime").description("endEventDateTime of New Event"),
+          fieldWithPath("location").description("location of New Event"),
+          fieldWithPath("basePrice").description("basePrice of New Event"),
+          fieldWithPath("maxPrice").description("maxPrice of New Event"),
+          fieldWithPath("limitOfEnrollment").description("limitOfEnrollment of New Event"),
+          fieldWithPath("free").description("free of New Event"),
+          fieldWithPath("offline").description("offline of New Event"),
+          fieldWithPath("eventStatus").description("eventStatus of New Event"),
+          fieldWithPath("_links.self.href").description("link to self"),
+          fieldWithPath("_links.query-events.href").description("link to query events"),
+          fieldWithPath("_links.update-event.href").description("link to update an existing event")
+        )
+      ))
 //      .andExpect(jsonPath("_link.profile").exists())
     ;
   }
